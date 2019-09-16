45df2d725a38e5b8831df1277998febe0f7d1861
Jon Atxa <atxajon@gmail.com>
2019-09-16 20:15:10 +0200

    intial setup of app, including newsapi key and cration of news-article component

---
+++ index.js
@@ -0,0 +1,17 @@
+import {topHeadlinesUrl} from './newsApi.js';
+
+// By delaying this until the page is loaded the experience is more smooth. Do not block initial render with js.
+window.addEventListener('load', () => {
+  fetchNews();
+});
+
+async function fetchNews() {
+  const res = await fetch(topHeadlinesUrl);
+  const json = await res.json();
+  const main = document.querySelector('main');
+  json.articles.forEach(article => {
+    const el = document.createElement('news-article');
+    el.article = article;
+    main.appendChild(el);
+  });
+}
---
+++ newsApi.js
@@ -0,0 +1,5 @@
+const apiKey = '9d0bdde674cf4a228a0988419061cdd9';
+const topHeadlinesUrl =
+  'https://newsapi.org/v2/top-headlines?country=us&apiKey=' + apiKey;
+
+export { apiKey, topHeadlinesUrl };
