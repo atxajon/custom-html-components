4d4aaa7604a87d0d6494ff3c6e02849eab5ff613
Jon Atxa <atxajon@gmail.com>
2019-09-16 22:15:44 +0200

    uses shadow DOM to encapsulate the inner details of a component; it shields the inner implementation of this component from any external css, so we moved in the styles into the component itself

--- news-article.js
+++ news-article.js
@@ -1,7 +1,25 @@
 class NewsArticle extends HTMLElement {
+
+  constructor() {
+    super();
+    this.root = this.attachShadow({mode: 'open'});
+  }
   
   set pulledArticle(article) {
-    this.innerHTML = `
+    this.root.innerHTML = `
+    <style>
+    h2 {
+      font-family: Georgia, 'Times New Roman', Times, serif;
+    }
+    a,
+    a:visited {
+      text-decoration: none;
+      color: inherit;
+    }
+    img {
+      width: 100%;
+    }
+    </style>
     <a href="${article.url}">
     <h2>${article.title}</h2>
     <img src="${article.urlToImage ? article.urlToImage : ''}">
