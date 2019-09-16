bf0aedfdc14e1915ef576f5e3887e8ff6ac9a11b
Jon Atxa <atxajon@gmail.com>
2019-09-16 22:54:59 +0200

    turned the app into a pwa by adding a manifest file and service worker

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import {topHeadlinesUrl} from './newsApi.js';
 // By delaying this until the page is loaded the experience is more smooth. Do not block initial render with js.
 window.addEventListener('load', () => {
   fetchNews();
+  registerSW();
 });
 
 async function fetchNews() {
@@ -15,4 +16,14 @@ async function fetchNews() {
     el.pulledArticle = article;
     main.appendChild(el);
   });
+}
+
+async function registerSW() {
+  if ('serviceWorker' in navigator) {
+    try {
+      await navigator.serviceWorker.register('./sw.js');
+    } catch (e) {
+      console.log(`SW registration failed`);
+    }
+  }
 }
